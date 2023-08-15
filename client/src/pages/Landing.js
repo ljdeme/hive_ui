49ab61f0189e1ba6702e6a74c1ee9fb49cdedea1
ljdeme <julia_demetita@knights.ucf.edu>
2023-08-14 23:40:11 -0400

    Updated landing page responsiveness

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -15,12 +15,13 @@ function Landing() {
                             <div className= 'headerText' >
                                 <h1>Take Control.</h1>
                             </div>
-                        </div>
-                        <div className= 'bodyText' >
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. 
-                                Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. 
-                                Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque 
-                                nibh nibh, at maximus ante.</p>
+                        
+                            <div className= 'bodyText' >
+                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. 
+                                    Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. 
+                                    Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque 
+                                    nibh nibh, at maximus ante.</p>
+                            </div>
                         </div>
                     </div>
                     <div className='box-item-2'>
@@ -46,55 +47,9 @@ function Landing() {
                     </div>
                 </div>
                 <a href='/register'>
-                <button class='landing-trynow'>Try Now</button>
+                    <button class='landing-trynow'>Try Now</button>
                 </a>
             </div>
         </div>
     )
 }
-
-
-  {/* <div className='box-item'>
-                    <div className='landingText1Header'>
-                        <div className= 'headerText' >
-                            <h1>Take Control.</h1>
-                        </div>
-                    </div>
-                    <div className='LandingText1Body'>
-                        <div className= 'bodyText' >
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. 
-                                Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. 
-                                Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque 
-                                nibh nibh, at maximus ante.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='box-item'>
-                    <div className= 'landing_img-1'>
-                        <img className='landingimg-1' src={connect_img}  alt='modular'/>
-                    </div>
-                </div>
-                <div className='box-item'>
-                    <div className='landingText2Header'>
-                        <div className= 'headerText' >
-                            <h1>Fits to your needs.</h1>
-                        </div>
-                    </div>
-                    <div className='landingText2Body'>
-                        <div className= 'bodyText' >
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. 
-                                Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. 
-                                Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque 
-                                nibh nibh, at maximus ante.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='box-item'>
-                    <div className= 'landing_img-2'>
-                        <img className='landingimg-2' src={modular_img} alt='modular'/>
-                    </div>
-                </div>
-            </div>
-            <a href='/register'>
-                <button class='landing-trynow'>Try Now</button>
-            </a> */}
