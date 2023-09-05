0510624c4791cf21132e16fc21c0e28cd892b297
ljdeme <julia_demetita@knights.ucf.edu>
2023-09-04 22:28:22 -0400

    created dashboard containers and updating css to use root variables

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,36 @@
-import '../App.css';
+import '../css/dashboard.css';
 
 function Dashboard() {
   return (
     
-    <div className="App">
-      <header className="App-header">
-        <h1> IN DASH </h1>
-      </header>
+    <div className="dashboard">
+      <div className="dashboard-container">
+        <div className="dashboard-layout">
+
+          <div className="dashboard-top-flexbox">
+            <div className="dashboard-agent-list-item">
+                <p>Agent</p>
+            </div>
+            <div className="dashboard-map-item">
+                <p>Map</p>
+            </div>
+          </div>
+
+          <div className="dashboard-bottom-flexbox">
+            <div className="dashboard-topics-item">
+              <p>Topics</p>
+            </div>
+            <div className="dashboard-messages-item">
+              <p>Messages</p>
+            </div>
+            <div className="dashboard-extra-item">
+              <p>EXTRA</p>
+            </div>
+
+          </div> 
+
+        </div>{/* dashboard-layout*/}
+      </div>
     </div>
   );
 }
